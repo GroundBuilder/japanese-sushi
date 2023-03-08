d78fef7670c5ecf2687732e3f878ff04a49f732c
GroundBuilder <erik_rynnes@hotmail.com>
2023-03-08 09:59:57 +0000

    Creat a input function for user to send in sales data

--- run.py
+++ run.py
@@ -14,7 +14,30 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('japanese_sushi')
 
 order = SHEET.worksheet("sales")
-
 data = order.get_all_values()
 
-print(data[0])
+def get_all_sales_data():
+    """
+    User make input of how much the sold after 1pm.
+    """
+
+
+def check_validation_data():
+    """
+    The input of the sales data has to be correct.
+    If it's not correct, run a while loop till the 
+    correct input is made. Explane what typ of input come
+    and put out to the terminal what its needed.
+    """
+    print("Welcome, please enter todays lunch data.")
+    print("In the order: Omakase, Moriawase, Salmon, Vegetarian, Poké Bowl")
+    print("Separate the numbers by commas.")
+    print("Example: 38,30,25,20,24\n")
+
+    data_str = input("Enter todays sales here: \n")
+    print(f"You provided this: {data_str}")
+
+check_validation_data()
+
+
+
