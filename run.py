05d1e545c66acd835702304db9b5f2ac7836fd46
GroundBuilder <erik_rynnes@hotmail.com>
2023-03-08 10:26:16 +0000

    Validate function to check if it correct 5 values, else a errror messega

--- run.py
+++ run.py
@@ -20,16 +20,16 @@ def get_all_sales_data():
     """
     User make input of how much the sold after 1pm.
     """
-    print("Welcome, please enter todays lunch data.")      #Text for the user.
+    print("Welcome, please enter todays lunch data.")       #Text for the user.
     print("In the order: Omakase, Moriawase, Salmon, Vegetarian, Poké Bowl") 
     print("Separate the numbers by commas.")
     print("Example: 38,30,25,20,24\n")
 
-    data_str = input("Enter todays sales here: ")          # The user get a input line to write in.
-    print(f"You provided this: {data_str}")                # The user can see the input
+    data_str = input("Enter todays sales here: ")           # The user get a input line to write in.
+    print(f"You provided this: {data_str}")                 # The user can see the input
 
-    sales_data = data_str.split(",")                       # ake the input from a string to a list.
-    check_validation_data(sales_data)
+    sales_data = data_str.split(",")                        # Take the input from a string to a list.
+    check_validation_data(sales_data)                       # Pass data to the next function.
 
 
 def check_validation_data(values):
@@ -39,6 +39,15 @@ def check_validation_data(values):
     correct input is made. Explane what typ of input come
     and put out to the terminal what its needed.
     """
+    try:
+        if len(values) != 5:                                # Provide an error if user don't add 5 values.
+            raise ValueError(
+                f"Expected 5 values, you provided {len(values)}.\n"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}Please try again.")
+
+
     print(values)
 
 
