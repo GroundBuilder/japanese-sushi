8c66c22c88f86e76341901af64e796ccd3ca4b02
GroundBuilder <erik_rynnes@hotmail.com>
2023-03-11 06:53:33 +0000

    Fix. deleted white spaces detected from pip8.

--- run.py
+++ run.py
@@ -27,10 +27,10 @@ def get_enter_input():                         # Ask if user want to put in toda
         print(f"\nYou pressed: {result_del}\n")
         print("\nYou will enter now!\n")
         main()
-        
+
     elif result_del == "N":
         print("\nWelcome back next time!\n")
-        
+
     else:
         main()
 
@@ -42,7 +42,7 @@ def get_all_sales_data():
     while True:                                         # Makes a loop if user get the wrong input
 
         print("\nWelcome, please enter todays lunch data.")      # Text for the user.
-        print("In the order: Omakase, Moriawase, Salmon, Vegetarian, Poké Bowl") 
+        print("In the order: Omakase, Moriawase, Salmon, Vegetarian, Poké Bowl")
         print("Separate the 5 numbers by commas.")
         print("Example: 38,30,25,20,24\n")
 
@@ -50,11 +50,10 @@ def get_all_sales_data():
         print(f"You provided this: {data_str}")           # The user can see the input.
 
         sales_data = data_str.split(",")                  # Take the input from a string to a list.
-        
 
         if check_validation_data(sales_data):              # Pass data to the next function.
             print("Data is valid")
-            break                                            
+            break
 
     return sales_data
 
@@ -62,7 +61,7 @@ def get_all_sales_data():
 def check_validation_data(values):
     """
     The input of the sales data has to be correct.
-    If it's not correct, run a while loop till the 
+    If it's not correct, run a while loop till the
     correct input is made. Explane what typ of input come
     and put out to the terminal what its needed.
     """
@@ -81,7 +80,7 @@ def check_validation_data(values):
 
 def update_sales_worksheet(data):                       # Function to add data to worksheet
     """
-    Update  the sales worksheet, adds a new row in the worksheet 
+    Update  the sales worksheet, adds a new row in the worksheet
     with the list of data the user have provided.
     """
     print("Updating the sales worksheet...\n")
@@ -92,7 +91,7 @@ def update_sales_worksheet(data):                       # Function to add data t
 
 def update_sureplus_worksheet(data):                # Function to add surplus data to worksheet
     """
-    Update  the surplus worksheet, adds a new row in the worksheet 
+    Update  the surplus worksheet, adds a new row in the worksheet
     with the list of data the calculation have provided.
     """
     print("Updating the surlplus worksheet...\n")
@@ -103,7 +102,7 @@ def update_sureplus_worksheet(data):                # Function to add surplus da
 
 def update_premanuf_worksheet(data):                # Function to add premanuf data to worksheet
     """
-    Update  the premanuf worksheet, adds a new row in the worksheet 
+    Update  the premanuf worksheet, adds a new row in the worksheet
     with the list of data the calculation have provided.
     """
     print("Updating the premanuf worksheet...\n")
@@ -112,25 +111,27 @@ def update_premanuf_worksheet(data):                # Function to add premanuf d
     print("Your premanuf is updated successfully.\n")
 
 
-def calculateing_surplus_data(sales_row): 
+def calculateing_surplus_data(sales_row):
     """
     Definition of sureplus:
     - If positive, it indicate that they had to throw away sushi.
-    - If negative, it's indicate that costumer had to wait 
+    - If negative, it's indicate that costumer had to wait
     while they made more sushi.
 
     This function calculate how much that have been wasted or made this day.
     """
     print("Calculating the sureplus data...\n")
-    premanuf = SHEET.worksheet("premanuf").get_all_values() # Get the value of premanuf in the worksheet.
-    premanuf_row_last = premanuf[-1]                        # Get the last value of the list in premanuf.
-    
+
+    premanuf = SHEET.worksheet("premanuf").get_all_values()  # Get the value of premanuf in the worksheet.
+    premanuf_row_last = premanuf[-1]                         # Get the last value of the list in premanuf.
+
     surplus_data = []
     for premanuf, sales in zip(premanuf_row_last, sales_row):   # To handle the both list at the same time
         surplus = int(premanuf) - sales                         # Calculate surplus, premanuf comes first as a str, now its a int.
         surplus_data.append(surplus)
 
     return surplus_data
+
 
 def get_last_3_sales():
     """
@@ -139,20 +140,21 @@ def get_last_3_sales():
     """
     sales = SHEET.worksheet("sales")                # Get data from the worksheet
     columns = []                                    # Empty variable
-    for ind in range(1,6):                          # Loop throug numbers 1-5
-        column = sales.col_values(ind)              
+    for ind in range(1, 6):                          # Loop throug numbers 1-5
+        column = sales.col_values(ind)
         columns.append(column[-3:])                 # -3, To get the latest input from the workcheet
-    
+
     return columns                                  # Sett the value to the function
-        
+
+
 def calculate_premanuf_data(data):                  # Calculat the premanufactury value
     """
-    Finds the average premanufacturing of 3 and 
-    the value for each item. 
+    Finds the average premanufacturing of 3 and
+    the value for each item.
     Then adding aditional 5%
     """
     print("Calculating premanuf data...\n")
-    
+
     new_premauf_data = []
     for column in data:
         int_column = [int(num) for num in column]
@@ -192,7 +194,7 @@ def get_data_deleted():                         # Ask if user whant to delet the
 
     elif result_del == "N":
         print("\nWelcome back next salesday!!!\n")
-        
+
     else:
         print("Did not press (Y/N), try again.")
         get_data_deleted()
@@ -212,15 +214,16 @@ def check_validation_delete():                      # When function calls it del
 
 def main():
     data = get_all_sales_data()
-    sales_data = [int(num) for num in data]                 # Make the list values to numbers instead of strings value"
-    update_sales_worksheet(sales_data)                      # Pass data to the function upsate_sales_worksheet.
-    new_surplus_data = calculateing_surplus_data(sales_data) # Sett the values to the variable
-    update_sureplus_worksheet(new_surplus_data)             # Pass datat to the function
+    sales_data = [int(num) for num in data]                  # Make the list values to numbers instead of strings value"
+    update_sales_worksheet(sales_data)                       # Pass data to the function upsate_sales_worksheet.
+    new_surplus_data = calculateing_surplus_data(sales_data)  # Sett the values to the variable
+    update_sureplus_worksheet(new_surplus_data)              # Pass datat to the function
     sales_columns = get_last_3_sales()
     premanuf_data = calculate_premanuf_data(sales_columns)
     update_premanuf_worksheet(premanuf_data)
     premanuf_values = get_premanuf_values(premanuf_data)
     print(premanuf_values)
     get_data_deleted()
-    
+
+
 get_enter_input()
